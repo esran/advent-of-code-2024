665628903e356f8906b4434f66553ffbf5886f72
Sean Johnston <shamus@conical-effort.net>
2024-12-05 15:10:15 +0000

    Rework day 5 for performance

--- day05.py
+++ day05.py
@@ -1,19 +1,29 @@
-import math
+from bisect import bisect_left
 
 print("..loading day05")
+
+
+# see https://stackoverflow.com/a/2701189
+def bi_contains(lst, item):
+    """efficient `item in lst` for sorted lists"""
+    # if item is larger than the last its not in the list, but the bisect would
+    # find `len(lst)` as the index to insert, so check that first. Else, if the
+    # item is in the list then it has to be at index bisect_left(lst, item)
+    return (item <= lst[-1]) and (lst[bisect_left(lst, item)] == item)
 
 
 def get_page_ordering(data: list[str]) -> list[tuple[int, int]]:
     result = []
 
-    for i, line in enumerate(data):
+    for _, line in enumerate(data):
         if len(line) == 0:
             break
 
-        a,b = (int(x) for x in line.split("|"))
-        result.append((a,b))
+        a, b = (int(x) for x in line.split("|"))
+        result.append((a, b))
 
-    return result
+    # Sort the ordering for use with bi_contains for performance
+    return sorted(result)
 
 
 def get_updates(data: list[str]) -> list[list[int]]:
@@ -32,34 +42,38 @@ def get_updates(data: list[str]) -> list[list[int]]:
 
     return result
 
+
 def check_update_ordered(update: list[int], ordering: list[tuple[int, int]]) -> bool:
-    for index in range(len(update)):
-        for other in range(index+1, len(update)):
-            for order in ordering:
-                if order[0] == update[other] and order[1] == update[index]:
-                    return False
+    for index in range(len(update) - 1):
+        # If our ordering contains the reverse of what we
+        # have then the update is not ordered
+        if bi_contains(ordering, (update[index + 1], update[index])):
+            return False
 
     return True
 
+
 def get_middle_page(update: list[int]) -> int:
-    half = int(len(update)/2)
+    half = int(len(update) / 2)
     return update[half]
 
-def fix_ordering(update: list[int], ordering:list[tuple[int, int]]) -> list[int]:
+
+def fix_ordering(update: list[int], ordering: list[tuple[int, int]]) -> list[int]:
     while not check_update_ordered(update, ordering):
         update = do_ordering(update, ordering)
 
     return update
 
-def do_ordering(update: list[int], ordering:list[tuple[int, int]]) -> list[int]:
-    for i in range(len(update)-1):
-        for order in ordering:
-            if update[i] == order[1] and update[i+1] == order[0]:
-                x = update[i]
-                update[i] = update[i+1]
-                update[i+1] = x
+
+def do_ordering(update: list[int], ordering: list[tuple[int, int]]) -> list[int]:
+    for i in range(len(update) - 1):
+        if bi_contains(ordering, (update[i + 1], update[i])):
+            x = update[i]
+            update[i] = update[i + 1]
+            update[i + 1] = x
 
     return update
+
 
 def func1(data: list[str]):
     result = 0
@@ -72,6 +86,7 @@ def func1(data: list[str]):
             result += get_middle_page(update)
 
     return result
+
 
 def func2(data: list[str]):
     result = 0
